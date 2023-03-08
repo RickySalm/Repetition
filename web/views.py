9349410bcad2522bea80f266ce176b9a31044c7d
Radmir Salimov <radmir-prog@mail.ru>
2023-03-09 00:30:17 +0300

    + from search

--- web/views.py
+++ web/views.py
@@ -6,14 +6,19 @@ from web.models import Note
 
 def main_view(request):
     with_alerts = 'with_alerts' in request.GET
+    search = request.GET.get('search', None)
     notes = Note.objects.all()
 
     if with_alerts:
         notes = notes.filter(alert_send_at__isnull=False)
+
+    if search:
+        notes = notes.filter(title=search)
 
     return render(request, 'web/main.html', {
         'count': Note.objects.count(),
         'notes': notes,
         'with_alerts': with_alerts,
         'query_params': request.GET,
+        'search': request.GET.get('search')
     })
