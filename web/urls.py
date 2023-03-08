6496282d78c822c50c9ea7d9800b1012540be991
Radmir Salimov <radmir-prog@mail.ru>
2023-03-09 01:39:05 +0300

    + redirect

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from web.views import main_view
+from web.views import notes_view, main_view
 
 urlpatterns = [
     path('', main_view),
+    path('notes/', notes_view, name='notes_list'),
 ]
