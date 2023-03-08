90b729aeeba517bb27fe82fe73089a802914b892
Radmir Salimov <radmir-prog@mail.ru>
2023-03-09 00:07:02 +0300

    = filter link

--- web/views.py
+++ web/views.py
@@ -5,9 +5,15 @@ from web.models import Note
 
 
 def main_view(request):
+    with_alerts = 'with_alerts' in request.GET
     notes = Note.objects.all()
-    print(notes)
+
+    if with_alerts:
+        notes = notes.filter(alert_send_at__isnull=False)
+
     return render(request, 'web/main.html', {
         'count': Note.objects.count(),
         'notes': notes,
+        'with_alerts': with_alerts,
+        'query_params': request.GET,
     })
