a4835b987983aa0511376ba1a8ef055e586dec0a
Radmir Salimov <radmir-prog@mail.ru>
2023-03-09 16:05:29 +0300

    + note edit

--- web/urls.py
+++ web/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from web.views import notes_view, main_view, note_view, note_add_view
+from web.views import notes_view, main_view, note_view, note_edit_view
 
 urlpatterns = [
     path('', main_view),
     path('notes/', notes_view, name='notes_list'),
     path('notes/<int:id>/', note_view, name='note'),
-    path('note/add/', note_add_view, name='notes_add')
+    path('note/add/', note_edit_view, name='notes_add'),
+    path('note/<int:id>/edit', note_edit_view, name='notes_edit'),
 ]
