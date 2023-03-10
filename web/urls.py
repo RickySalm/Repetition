d1b2bfe054cdac7b573868c57a39e3316d3e5617
Radmir Salimov <radmir-prog@mail.ru>
2023-03-11 02:08:47 +0300

    + registration as class view

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from web.views import notes_view, main_view, note_view, note_edit_view, registration_view, login_view, logout_view
+from web.views import notes_view, main_view, note_view, note_edit_view, RegistrationView, login_view, logout_view
 
 urlpatterns = [
     path('', main_view, name='main'),
@@ -9,7 +9,7 @@ urlpatterns = [
     path('notes/<int:id>/', note_view, name='note'),
     path('note/add/', note_edit_view, name='notes_add'),
     path('note/<int:id>/edit', note_edit_view, name='notes_edit'),
-    path('registration/', registration_view, name='registration'),
+    path('registration/', RegistrationView.as_view(), name='registration'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
 ]
