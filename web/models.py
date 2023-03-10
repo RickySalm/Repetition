1b0a4aeae94a0c9a33641d38eb8e786c75ece92a
Radmir Salimov <radmir-prog@mail.ru>
2023-03-11 01:39:35 +0300

    + custom user model

--- web/models.py
+++ web/models.py
@@ -1,8 +1,9 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import UserManager as DjangoUserManager
 from django.db import models
 
-
-User = get_user_model()
+from web.enums import Role
 
 
 class BaseModels(models.Model):
@@ -11,6 +12,43 @@ class BaseModels(models.Model):
 
     class Meta:
         abstract = True
+
+
+class UserManager(DjangoUserManager):
+
+    def _create_user(self, username, email, password, commit=True, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        if commit:
+            user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(self, email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        return self._create_user(self, email, password, role=Role.admin, **extra_fields)
+
+
+class User(BaseModels, AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(choices=Role.choices, max_length=15)
+    name = models.CharField(max_length=255, null=True, blank=True)
+
+    @property
+    def is_staff(self):
+        return self.role in (Role.admin, Role.staff)
+
+    @property
+    def is_superuser(self):
+        return self.role == Role.admin
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 
 class Tag(BaseModels):
